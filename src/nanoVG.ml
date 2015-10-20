473dfca4f98e5752de7278fcbdc803f18132c0ca
Rizo Isrof <rizo@odis.io>
2015-10-20 23:22:09 +0100

    Working rev with fixed array field.

--- src/nanoVG.ml
+++ src/nanoVG.ml
@@ -2,17 +2,61 @@
 open Ctypes
 open Foreign
 
+let discard x = ()
+
+let array_field st name t n =
+  let r = field st name t in
+  for i = 0 to (n - 1) do
+    discard (field st name t)
+  done; r
+
+let antialias = 1 lsl 0
+let stencil_strokes	= 1 lsl 1
+let debug = 1 lsl 2
 
 type context = unit ptr
 let context : context typ = ptr void
 
 type color
-let color : color structure typ = structure "color"
-let r = field color "r" float
-let g = field color "g" float
-let b = field color "b" float
-let a = field color "a" float
+let color : color structure typ = structure "NVGcolor"
+let color_r = field color "r" float
+let color_g = field color "g" float
+let color_b = field color "b" float
+let color_a = field color "a" float
 let () = seal color
+
+type paint = {
+  xform       : float list;
+  extent      : float list;
+  radius      : float;
+  feather     : float;
+  inner_color : color;
+  outer_color : color;
+  image       : int;
+}
+
+let paint : paint structure typ = structure "NVGpaint"
+
+let paint_xform       = array_field paint "xform" float 6
+let paint_extent      = array_field paint "extent" float 2
+let paint_radius      = field paint "radius" float
+let paint_feather     = field paint "feather" float
+let paint_inner_color = field paint "innerColor" color
+let paint_outer_color = field paint "outerColor" color
+let paint_image       = field paint "image" int
+let ()                = seal paint
+
+let rgba =
+  foreign "nvgRGBA"
+    (int @-> int @-> int @-> int @-> returning color)
+
+let rgb =
+  foreign "nvgRGB"
+    (int @-> int @-> int @-> returning color)
+
+let rgbaf =
+  foreign "nvgRGBAf"
+    (float @-> float @-> float @-> float @-> returning color)
 
 let null = Ctypes.null
 
@@ -23,7 +67,7 @@ let cancel_frame =
   foreign "nvgCancelFrame" (context @-> returning void)
 
 let end_frame =
-  foreign "nvgEndFrame" (context @-> int @-> int @-> float @-> returning void)
+  foreign "nvgEndFrame" (context @-> returning void)
 
 let begin_path =
   foreign "nvgBeginPath" (context @-> returning void)
@@ -31,13 +75,41 @@ let begin_path =
 let fill =
   foreign "nvgFill" (context @-> returning void)
 
+let rect =
+  foreign "nvgRect" (context @-> float @-> float @-> float @-> float
+                             @-> returning void)
+
 let round_rect =
   foreign "nvgRoundedRect" (context @-> float @-> float @-> float @-> float
                                     @-> float @-> returning void)
 
+let ellipse =
+  foreign "nvgEllipse" (context @-> float @-> float @-> float @-> float
+                                @-> returning void)
+
 let fill_color =
   foreign "nvgFillColor" (context @-> color @-> returning void)
 
+let create_image =
+  foreign "nvgCreateImage" (context @-> string @-> int @-> returning int)
+
+let image_size_c =
+  foreign "nvgImageSize" (context @-> int @-> ptr int @-> ptr int @-> returning void)
+
+let get_image_size vg img_id =
+  let w = allocate int 0 in
+  let h = allocate int 0 in
+  image_size_c vg img_id w h;
+  !@ w, !@ h
+
+let image_pattern =
+  foreign "nvgImagePattern" (context @-> float @-> float @-> float @-> float
+                                     @-> float @-> int @-> float @-> returning paint)
+
+let fill_paint =
+  foreign "nvgFillPaint" (context @-> paint @-> returning void)
+
 let create_gl3 =
   foreign "nvgCreateGL3" (int @-> returning context)
+
 
--- src/test_nanovg.ml
+++ src/test_nanovg.ml
@@ -3,19 +3,59 @@ open Tgl3
 
 module Vg = NanoVG
 
+let fmt = Printf.sprintf
+
 let () =
-  let _ = Glfw.init () in
-  let win = Glfw.create_window 800 600 "GLFW OCaml Demo" Glfw.null Glfw.null in
+  let init_status = Glfw.init () in
+  print_endline (fmt "init_status: %d" init_status);
+  (if init_status <> 1 then
+     exit 1);
+
+  Glfw.window_hint Glfw.context_version_major 3;
+  Glfw.window_hint Glfw.context_version_minor 2;
+  Glfw.window_hint Glfw.opengl_forward_compat 1;
+  Glfw.window_hint Glfw.opengl_profile Glfw.opengl_core_profile;
+
+  let win = Glfw.create_window 1483 748 "GLFW OCaml Demo" Glfw.null Glfw.null in
   Glfw.make_context_current win;
+
+  let vg = Vg.(create_gl3 (antialias lor stencil_strokes lor debug)) in
+  let c = Vg.rgba 255 0 0 255 in
+
+  Gl.viewport 0 0 1483 748;
+  Gl.clear_color 0.5 0.5 0.6 1.0;
+  Gl.clear (Gl.color_buffer_bit lor Gl.depth_buffer_bit lor Gl.stencil_buffer_bit);
+
+  let img = Vg.create_image vg "/Users/rizo/Desktop/9.jpg" 0 in
+  let (iw, ih) =
+    let iw_int, ih_int = Vg.get_image_size vg img in
+    (float_of_int iw_int, float_of_int ih_int) in
+  print_endline (fmt "image size: (%f, %f)\n" iw ih);
+  let img_paint = Vg.image_pattern vg 0.0 0.0 iw ih (0.0 /. 180.0 *. 3.14) img 1.0 in
+  let draw_bg () =
+    Vg.begin_path vg;
+    Vg.rect vg 0.0 0.0 iw ih;
+    Vg.fill_paint vg img_paint;
+    Vg.fill vg in
+  draw_bg ();
+
   Glfw.swap_interval 0;
-  while not (Glfw.window_should_close win <> 0) do
-    Gl.viewport 0 0 800 600;
-    Gl.clear_color 0.5 0.5 0.9 1.;
-    Gl.clear Gl.color_buffer_bit;
 
-    let ctx = Vg.create_gl3 0 in
-    Vg.begin_frame ctx 800 600 1.0;
+  while Glfw.window_should_close win = 0 do
+    let (mx, my) = Glfw.get_cursor_pos win in
 
+    draw_bg();
+
+    Vg.begin_frame vg 1483 748 1.0;
+
+    Vg.begin_path vg;
+    Vg.ellipse vg mx my 50. 50.;
+    Vg.fill_color vg c;
+    Vg.fill vg;
+
+    Vg.end_frame vg;
+
+    Gl.flush ();
     Glfw.swap_buffers win;
     Glfw.poll_events ();
   done
